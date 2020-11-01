1cef76d03e8b8f7c8204f46aef98081e9dd47f91
Floaterest <floaterest@outlook.jp>
2020-10-31 19:16:43 -0500

    Add vtt parser

--- vtt.ts
+++ vtt.ts
@@ -1,33 +1,38 @@
-function toSeconds(t: string) {
-    let s = 0.0;
-    if (t) {
-        for (let p of t.split(':')) {
-            s = s * 60 + parseFloat(p);
-        }
-    }
-    return s;
+interface Subtitle {
+    start: number;
+    end: number;
+    subtitle: string;
 }
 
-function strip(s: string) {
-    return s.replace(/^\s+|\s+$/g, '');
-}
+class VTT {
+    subtitles: Subtitle[] = [];
+    constructor(vtt: string) {
 
-var subtitles: { [id: number]: {} } = {};
-var vtt;
-function play(element: JQuery) {
-    vtt = element.text().replace(/\r\n|\r|\n/g, '\n');
-    element.text('');
+    }
 
-    let a: string[];
-    let start: string, end: string, sub: string;
-    for (let s of vtt.split('\n\n').slice(1)) {
-        a = s.split('\n');
-        if (a.length > 1) {
-            [start, end] = a[0].split(' --> ');
-            sub = a.slice(1).join('\n')
+    private toSeconds(t: string): number {
+        let s = 0.0
+        t.split(':').forEach(p => s = s * 60 + parseFloat(p));
+        return s;
+    }
 
-            subtitles[toSeconds(start)] = { start: start, end: end, subtitle: sub };
+    parse(vtt: string) {
+        let lines: string[];
+        let start: number, end: number;
+        // slice(1) because the first line is 'WEBVTT'
+        for (let cue of vtt.split('\n\n').map(c => c.trim()).slice(1)) {
+            lines = cue.split('\n');
+            // first line is the timestamp
+            [start, end] = lines.shift()!
+                .split('  -->  ')
+                .map(l => this.toSeconds(l));
+
+            this.subtitles.push({
+                start: start,
+                end: end,
+                // the rest is subtitle
+                subtitle: lines.join('\n')
+            });
         }
     }
-    console.log(subtitles);
-} 
+}

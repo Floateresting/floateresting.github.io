fb02ef891d255afa1c9e64f06cf7c728072f77fd
Floaterest <floaterest@outlook.jp>
2020-11-05 17:08:31 -0500

    Finish vtt parser

--- vtt.ts
+++ vtt.ts
@@ -86,3 +86,27 @@ class VTT {
         }, this.int * 1000);
     }
 }
+
+function strip(s: string) {
+    return s.replace(/^\s+|\s+$/g, '');
+}
+
+var subtitles: { [id: number]: {} } = {};
+var vtt;
+function play(element: JQuery) {
+    vtt = element.text().replace(/\r\n|\r|\n/g, '\n');
+    element.text('');
+
+    let a: string[];
+    let start: string, end: string, sub: string;
+    for (let s of vtt.split('\n\n').slice(1)) {
+        a = s.split('\n');
+        if (a.length > 1) {
+            [start, end] = a[0].split(' --> ');
+            sub = a.slice(1).join('\n')
+
+            subtitles[toSeconds(start)] = { start: start, end: end, subtitle: sub };
+        }
+    }
+    console.log(subtitles);
+}
